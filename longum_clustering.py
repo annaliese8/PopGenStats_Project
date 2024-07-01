470702d86ccb7f1798caa11c4115c71677f34389
annaliese8 <annalieseferchau@gmail.com>
2024-07-01 10:06:50 +0800

    filter for selected columns in longum matrix

--- longum_clustering.py
+++ longum_clustering.py
@@ -1,6 +1,7 @@
 import seaborn
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 file_path = '/home/ubuntu/wdir/PopGenStats_Project/data/BifidoIsolates/BLongum/skani_ani_edge_list.tsv'
 skani_matrix = pd.read_csv(file_path, sep='\t')
@@ -11,6 +12,10 @@ skani_matrix = pd.read_csv(file_path, sep='\t')
 #     values='ANI'
 # )
 # pivot_table = pivot_table.fillna(0)
+
+skani_matrix = np.array(skani_matrix)[:,[2,3,4]]
+
+# print("The Kth column of matrix is : " + str(res))
 
 seaborn.clustermap(skani_matrix)
 plt.show()
