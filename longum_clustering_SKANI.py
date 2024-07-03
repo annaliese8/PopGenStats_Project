fda391beeadd3675bf9fb5b2069c2a591f20237a
annaliese8 <annalieseferchau@gmail.com>
2024-07-03 08:48:07 +0800

    revised SKANI clustering file, built clustermap for mummer data

---
+++ longum_clustering_SKANI.py
@@ -0,0 +1,20 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# file_path = '/home/ubuntu/wdir/PopGenStats_Project/data/BifidoIsolates/BLongum/skani_ani_edge_list.tsv.gz'
+file_path = '/Users/Annaliese/Desktop/PopGenStats_Project/data/BifidoIsolates/BLongum/skani_ani_edge_list.tsv.gz'
+
+columns_to_use = ['Ref_file', 'Query_file', 'ANI']
+chunk_list = []
+
+for chunk in pd.read_csv(file_path, sep = '\t', usecols = columns_to_use, chunksize = 10000):
+    chunk_list.append(chunk)
+
+skani_matrix = pd.concat(chunk_list)
+
+skani_pivot = skani_matrix.pivot(index = 'Ref_file', columns = 'Query_file', values = 'ANI')
+skani_pivot = skani_pivot.fillna(0)
+
+sns.clustermap(skani_pivot)
+plt.show()
---
+++ longum_clustering_mummer.py
@@ -0,0 +1,20 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# file_path = '/home/ubuntu/wdir/PopGenStats_Project/data/BifidoIsolates/BLongum/mummer.tsv'
+file_path = '/Users/Annaliese/Desktop/PopGenStats_Project/data/BifidoIsolates/BLongum/mummer.tsv'
+
+columns_to_use = ['prc_aligned1', 's1', 's2']
+chunk_list = []
+
+for chunk in pd.read_csv(file_path, sep = '\t', usecols = columns_to_use, chunksize = 10000):
+    chunk_list.append(chunk)
+
+skani_matrix = pd.concat(chunk_list)
+
+skani_pivot = skani_matrix.pivot(index = 's1', columns = 's2', values = 'prc_aligned1')
+skani_pivot = skani_pivot.fillna(0)
+
+sns.clustermap(skani_pivot)
+plt.show()
