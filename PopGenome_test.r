a8948363bdfff8a7e60d905bb3618bb82d33d601
annaliese8 <annalieseferchau@gmail.com>
2024-10-28 11:32:02 +0800

    removed unused file

--- PopGenome_test.r
+++
@@ -1,7 +0,0 @@
-library(PopGenome)
-
-GENOME.class <- readData("/home/ubuntu/wdir/PopGenStats_Project/output_infantis_genomes/infantis_genomes/")
-GENOME.class <- diversity.stats(GENOME.class)
-
-#show the result:
-GENOME.class@nuc.diversity.within
